255cdc0a3016bbdeab94f2b20187453280555484
michaelmccarty <mccartyx1@gmail.com>
2019-09-16 01:29:05 -0400

    Hid comments if none were found

--- public/main.js
+++ public/main.js
@@ -7,12 +7,18 @@ $(document).ready(function () {
     $.get("/articles").done(function (data) {
         for (let i = 0; i < data.length; i++) {
             //<div class="newsArticle shadow-lg p-3 mb-5 bg-white rounded">
+            let lastComment = data[i].lastComment;
+
+            if (typeof lastComment === 'undefined') {
+                lastComment = "";
+            }
+
             newsCards.append(`
                 <div class="newsArticle card text-center">
                 <div class="card-body">
                 <h5 class="card-title"><a href="${data[i].link}"><strong>${data[i].title}</strong></a></h5>
-                <p class="card-text">click to view comment activity</p>
-                <p class="card-text"><small class="text-muted">Last comment 3 mins ago</small></p>
+                <p class="card-text"><small class="text-muted">${lastComment}</small></p>
+                <p class="card-text"><button type="button" class="btn btn-link">comment</button></p>
                 </div>
             `);
         }
