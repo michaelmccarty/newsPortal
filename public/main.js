503a3163d5531e24cde28f3ad62bfc1a51df426e
michaelmccarty <mccartyx1@gmail.com>
2019-09-16 18:40:41 -0400

    Added modals for user commenting.

--- public/main.js
+++ public/main.js
@@ -2,8 +2,6 @@ $(document).ready(function () {
     let content = $(".content");
     let newsCards = $(".newsCards")
 
-
-
     $.get("/articles").done(function (data) {
         for (let i = 0; i < data.length; i++) {
             //<div class="newsArticle shadow-lg p-3 mb-5 bg-white rounded">
@@ -18,7 +16,7 @@ $(document).ready(function () {
                 <div class="card-body">
                 <h5 class="card-title"><a href="${data[i].link}"><strong>${data[i].title}</strong></a></h5>
                 <p class="card-text"><small class="text-muted">${lastComment}</small></p>
-                <p class="card-text"><button type="button" class="btn btn-link">comment</button></p>
+                <p class="card-text"><button type="button" class="btn btn-link" data-toggle="modal" data-target="#exampleModalCenter">comment</button></p>
                 </div>
             `);
         }
