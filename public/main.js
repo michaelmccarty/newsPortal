93c6bef4a568cf7b5bfb7acaaadfee194da56682
michaelmccarty <mccartyx1@gmail.com>
2019-09-15 22:37:42 -0400

    Added mobile first design for responsiveness.

--- public/main.js
+++ public/main.js
@@ -1,11 +1,20 @@
 $(document).ready(function () {
     let content = $(".content");
+    let newsCards = $(".newsCards")
 
 
 
     $.get("/articles").done(function (data) {
         for (let i = 0; i < data.length; i++) {
-            content.append(`<div class="newsArticle shadow-lg p-3 mb-5 bg-white rounded"><a href="${data[i].link}"><strong>${data[i].title}</strong></a></div>`);
+            //<div class="newsArticle shadow-lg p-3 mb-5 bg-white rounded">
+            newsCards.append(`
+                <div class="newsArticle card text-center">
+                <div class="card-body">
+                <h5 class="card-title"><a href="${data[i].link}"><strong>${data[i].title}</strong></a></h5>
+                <p class="card-text">click to view comment activity</p>
+                <p class="card-text"><small class="text-muted">Last comment 3 mins ago</small></p>
+                </div>
+            `);
         }
     })
 
