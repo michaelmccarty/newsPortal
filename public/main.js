2b177998cb363f8641d466e08f72f982e61bc2f9
michaelmccarty <mccartyx1@gmail.com>
2019-09-16 22:33:44 -0400

    Added id's to article divs.

--- public/main.js
+++ public/main.js
@@ -2,6 +2,13 @@ $(document).ready(function () {
     let content = $(".content");
     let newsCards = $(".newsCards")
 
+
+    $("#post").click(function(){
+        console.log("hi");
+    });
+
+
+
     $.get("/articles").done(function (data) {
         for (let i = 0; i < data.length; i++) {
             //<div class="newsArticle shadow-lg p-3 mb-5 bg-white rounded">
@@ -12,7 +19,7 @@ $(document).ready(function () {
             }
 
             newsCards.append(`
-                <div class="newsArticle card text-center">
+                <div class="newsArticle card text-center" id="${data[i]._id}">
                 <div class="card-body">
                 <h5 class="card-title"><a href="${data[i].link}"><strong>${data[i].title}</strong></a></h5>
                 <p class="card-text"><small class="text-muted">${lastComment}</small></p>
